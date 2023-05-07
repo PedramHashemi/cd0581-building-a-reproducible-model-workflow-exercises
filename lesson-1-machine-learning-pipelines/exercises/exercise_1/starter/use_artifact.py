b97e1a7e730ce71bdecef1a4e8d09896f4be5459
PedramHashemi <s.pedram.h@gmail.com>
2023-05-07 18:27:09 +0200

    update lesson 1

--- lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/use_artifact.py
+++ lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/use_artifact.py
@@ -17,6 +17,7 @@ def go(args):
     logger.info("Getting artifact")
 
     # YOUR CODE HERE: get the artifact and store its local path in the variable "artifact_path"
+    artifact = run.use_artifact(args.artifact_name)
     # HINT: you can get the artifact path by using the "file()" method
 
     artifact_path = artifact.file()
